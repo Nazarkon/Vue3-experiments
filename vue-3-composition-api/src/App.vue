532d93a13a03774903bfe64ea753b6d6577516cf
Nazar Montsibovych <nazar_montsibovych@epam.com>
2023-12-20 18:17:09 +0200

    simple example of using vue 3 features

--- vue-3-composition-api/src/App.vue
+++ vue-3-composition-api/src/App.vue
@@ -1,30 +1,57 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <h1>{{ name }}</h1>
+  <input type="text" v-model="name" />
+  <button @click="placeOrder">Place Order</button>
+  <button @click="removeWatcher">Hide Cart Alerts</button>
+  <YummyMeal
+    v-for="(meal,index) in meals"
+    :key="index"
+    :name="meal.name"
+    :price="meal.price"
+    @addToCart="addItemToCart"
+  />
+<br><br>
+  <select v-model="currencySymbol">
+    <option value="$">Dollar($)</option>
+    <option value="#">Dollar 2 (#)</option>
+  </select>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+<script>
+import YummyMeal from "./components/YummyMeal.vue";
+import { ref, reactive, watch, provide } from "vue";
+export default {
+  components: { YummyMeal },
+  setup() {
+    const currencySymbol = ref("$")
+    provide('currencySymbol', currencySymbol)
+    const name = ref("The Snazzy Burger");
+    const cart = reactive([]);
+    const meal = reactive({ name: "Hamburger 🍔", price: 5 });
+    const meals = reactive([
+      { name: "Hamburger 🍔", price: 5 },
+      { name: "Cheeseburger 🧀", price: 6 },
+      { name: "Impossible Burger 🥕", price: 7 },
+      { name: "Fries 🍟", price: 2 },
+    ]);
+    const placeOrder = () => alert("Your order has been placed!");
+    const addItemToCart = (item) => cart.push(item);
+    const removeWatcher = watch([() => [...cart]], (newValue, oldValue) =>
+      alert(newValue.join("\n"))
+    );
+
+    return { name, placeOrder, addItemToCart, meal, meals, removeWatcher, currencySymbol };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
 }
 </style>
---
+++ vue-3-composition-api/src/components/YummyMeal.vue
@@ -0,0 +1,26 @@
+<template>
+   <p>
+    <strong>{{ name }}</strong>
+    <YummyMealPrice :price="price"/>
+   </p>
+
+   <button @click="addToCart">Add to Cart</button>
+</template>
+<script>
+import YummyMealPrice from './YummyMealPrice.vue';
+
+export default {
+    components: { YummyMealPrice },
+    props: {
+        name: String,
+        price: Number,
+    },
+    setup(props, { emit }) {
+        console.log(props.name);
+        console.log(props.price);
+        const addToCart = () => emit("addToCart", props.name);
+        return { addToCart };
+    },
+    components: { YummyMealPrice }
+}
+</script>
